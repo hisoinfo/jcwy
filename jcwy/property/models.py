ee96855d7faa2ec5bd9690c9b505080a24360907
willschang <zhhu245@qq.com>
2015-07-01 23:48:10 +0800

    test

--- jcwy/property/models.py
+++ jcwy/property/models.py
@@ -8,6 +8,7 @@ class Members(models.Model):
     member_name = models.CharField(null=False, max_length=20, help_text='用户姓名')
     pwd = models.CharField(null=False, max_length=20, help_text='用户密码')
     regist_date = models.DateTimeField(auto_now_add=True)
-
+    
+    #返回用户名S
     def __unicode__(self):
     	return self.member_name
